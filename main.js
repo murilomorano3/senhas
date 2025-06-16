7c61586999dfc9ad7ad85f601b6821bb3623048d
murilomorano3 <133981366+murilomorano3@users.noreply.github.com>
2025-06-16 20:24:48 +0000

    a

---
+++ main.js
@@ -0,0 +1,7 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+
+numeroSenha.textContent = 5;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes);
