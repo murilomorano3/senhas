b15ad6ae1d9679ce65dbeec5201dbf5c0df93226
murilomorano3 <133981366+murilomorano3@users.noreply.github.com>
2025-06-23 23:59:21 +0000

    a

--- main.js
+++ main.js
@@ -6,9 +6,15 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho (){
     tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function (){
+    tamanhoSenha = tamanhoSenha+1;
     numeroSenha.textContent = tamanhoSenha;
 }
 console.log(botoes);
