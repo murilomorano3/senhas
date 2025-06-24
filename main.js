4ee9c29516f206124528db0a39e6b4bbeea9a3ec
murilomorano3 <133981366+murilomorano3@users.noreply.github.com>
2025-06-24 00:39:34 +0000

    a

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-
 numeroSenha.textContent = tamanhoSenha;
-
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const forcaSenha = document.querySelector('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -24,17 +29,9 @@ function aumentaTamanho (){
     geraSenha();
 }
 
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox');
-
 for (i=0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
-
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
-const numeros = '0123456789';
-const simbolos = '!@%*?';
 
 geraSenha();
 
@@ -52,7 +49,6 @@ function geraSenha (){
     if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
-    console.log(alfabeto);
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random()*alfabeto.length;
@@ -60,4 +56,17 @@ function geraSenha (){
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
+    classificaSenha();
+}
+
+function classificaSenha (){
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
+    if (tamanhoSenha > 11){
+        forcaSenha.classList.add('forte');
+    }else if (tamanhoSenha > 5 && tamanhoSenha < 12){
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <= 5) {
+        forcaSenha.classList.add('fraca');
+    }
+    
 }
