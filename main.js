d9ffd4be0b0eb2404dc9cf3ac041671db77ab55f
murilomorano3 <133981366+murilomorano3@users.noreply.github.com>
2025-06-24 00:53:10 +0000

    a

--- main.js
+++ main.js
@@ -70,5 +70,5 @@ function classificaSenha (tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = Math.floor(entropia)/(100e6*60*60*24);
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha.";
 }
